122dc483110d0237a30f2b23c173c9f64062f8f1
Prakriti <150656329+prakritea@users.noreply.github.com>
2025-03-24 10:06:49 +0530

    Add files via upload

---
+++ 2.data type and variabple.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a= 34;
+    cout <<a <<endl;
+
+    // float b = 45.66;
+    // cout <<b << endl;
+
+    // char c = 'a';
+    // cout<<c <<endl;
+
+    // bool d = 1;
+    // cout <<d <<endl;
+
+    // double e = 4.456;
+    // cout <<d <<endl;
+
+    int size = sizeof(a);
+    cout <<size <<endl;
+
+    // char h = 98; //important
+    // cout << h <<endl; //ASCII Value
+}
