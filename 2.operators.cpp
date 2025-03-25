a02bfbc8cbacc2cbac162adf266fd4db457a72ed
Prakriti <150656329+prakritea@users.noreply.github.com>
2025-03-25 11:32:14 +0530

    Add files via upload

---
+++ 2.operators.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int a = 2/5;
+    // cout <<a <<endl;
+
+    // float b = 2.0/5;
+    // cout << b <<endl;
+
+    int x= 2;
+    int y=4;
+
+    bool first = (x==y);
+    cout << first <<endl;
+
+    bool second = (x>=y);
+    cout <<second <<endl;
+
+    bool third = (x<=y);
+    cout <<third <<endl;
+
+
+}
